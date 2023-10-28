b5a0d4ee29436b4a989da5897fd0d3b1fb033730
Santiago Toro <30513187+storoor@users.noreply.github.com>
2023-10-27 22:54:41 -0500

    creacion de nuevo repositorio
    nuevo repositorio, problemas con el otro

---
+++ pages/urls.py
@@ -0,0 +1,34 @@
+from django.urls import path
+from .views import (
+    HomePageView,
+    AboutPageView,
+    ContactPageView,
+    ProductIndexView,
+    ProductShowView,
+    ProductCreateView,
+    CartView,
+    CartRemoveAllView,
+    ImageViewFactory,
+    ImageViewNoDI,
+)
+from .utils import ImageLocalStorage
+
+
+urlpatterns = [
+    path("", HomePageView.as_view(), name="home"),
+    path("about/", AboutPageView.as_view(), name="about"),
+    path("contact/", ContactPageView.as_view(), name="contact"),
+    path("products/", ProductIndexView.as_view(), name="products"),
+    path("products/create", ProductCreateView.as_view(), name="form"),
+    path("products/product_created", ProductCreateView.as_view(), name="product_created"),
+    path("products/<str:id>", ProductShowView.as_view(), name="show"),
+    path("cart/", CartView.as_view(), name="cart_index"),
+    path("cart/add/<str:product_id>", CartView.as_view(), name="cart_add"),
+    path("cart/removeAll", CartRemoveAllView.as_view(), name="cart_removeAll"),
+    path("image/", ImageViewFactory(ImageLocalStorage()).as_view(), name="image_index"),
+    path(
+        "image/save", ImageViewFactory(ImageLocalStorage()).as_view(), name="image_save"
+    ),
+    path("imagenotdi/", ImageViewNoDI.as_view(), name="imagenodi_index"),
+    path("image/save", ImageViewNoDI.as_view(), name="imagenodi_save"),
+]
